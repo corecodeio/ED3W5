a2f6f61f5aa621356c5bf24e87ac57756496582c
Yosef <yosefmarr@gmail.com>
2022-02-09 19:16:35 -0600

    first commit

---
+++ src/Main.ts
@@ -0,0 +1,11 @@
+// import { Input, Choice, SelectChoice } from './Input';  /* This are the different options to get input from a user */
+/* YOUR CODE HERE */
+
+export class Main {
+  // movies: Movie[] = []; /* This is array you need to use */
+  /* YOUR CODE HERE */
+
+  async start() {
+    /* YOUR CODE HERE */
+  }
+}
---
+++ src/Movie.ts
@@ -0,0 +1,2 @@
+// import { v4 as uuidv4 } from 'uuid';  /* This is the package to generate unique ids */
+/* YOUR CODE HERE */
